5cb4136839bc4e3ac72f5a8538fc68889f646eda
PrabalParihar <prabalparihar95@gmail.com>
2024-06-05 14:24:00 +0530

    ```text feat: Add Mint function to TokenERC20Contract
    Implement the Mint function in the TokenERC20Contract to allow authorized clients to mint new tokens. The function checks if the contract is already initialized and if the client is authorized to mint tokens. It updates the minter's balance and the total token supply in the world state. The function also emits a "Transfer" event to signal the minting.

--- Contracts/token/ERC20.go
+++ Contracts/token/ERC20.go
@@ -61,3 +61,86 @@ func (c *TokenERC20Contract) Initialize(ctx kalpsdk.TransactionContextInterface,
 
 	return true, nil
 }
+
+func (c *TokenERC20Contract) Mint(ctx kalpsdk.TransactionContextInterface, amount int) error {
+	initialized, err := checkInitialized(ctx)
+	if err != nil {
+		return fmt.Errorf("failed to check if contract is already initialized: %v", err)
+	}
+	if !initialized {
+		return fmt.Errorf("contract options need to be set before calling any function, call Initialize() to initialize contract")
+	}
+
+	clientMSPID, err := ctx.GetClientIdentity().GetMSPID()
+	if err != nil {
+		return fmt.Errorf("failed to get MSPID: %v", err)
+	}
+	if clientMSPID != "mailabs" {
+		return fmt.Errorf("client is not authorized to mint new tokens")
+	}
+
+	minter, err := ctx.GetUserID()
+	if err != nil {
+		return fmt.Errorf("failed to get client id: %v", err)
+	}
+
+	if amount <= 0 {
+		return fmt.Errorf("mint amount must be a positive integer")
+	}
+
+	currentBalanceBytes, err := ctx.GetState(minter)
+	if err != nil {
+		return fmt.Errorf("failed to read minter account %s from world state: %v", minter, err)
+	}
+
+	var currentBalance int
+	if currentBalanceBytes == nil {
+		currentBalance = 0
+	} else {
+		currentBalance, _ = strconv.Atoi(string(currentBalanceBytes))
+	}
+
+	updatedBalance, err := add(currentBalance, amount)
+	if err != nil {
+		return err
+	}
+
+	err = ctx.PutStateWithoutKYC(minter, []byte(strconv.Itoa(updatedBalance)))
+	if err != nil {
+		return err
+	}
+
+	totalSupplyBytes, err := ctx.GetState(totalSupplyKey)
+	if err != nil {
+		return fmt.Errorf("failed to retrieve total token supply: %v", err)
+	}
+
+	var totalSupply int
+	if totalSupplyBytes == nil {
+		totalSupply = 0
+	} else {
+		totalSupply, _ = strconv.Atoi(string(totalSupplyBytes))
+	}
+
+	totalSupply, err = add(totalSupply, amount)
+	if err != nil {
+		return err
+	}
+
+	err = ctx.PutStateWithoutKYC(totalSupplyKey, []byte(strconv.Itoa(totalSupply)))
+	if err != nil {
+		return err
+	}
+
+	transferEvent := event{"0x0", minter, amount}
+	transferEventJSON, err := json.Marshal(transferEvent)
+	if err != nil {
+		return fmt.Errorf("failed to obtain JSON encoding: %v", err)
+	}
+	err = ctx.SetEvent("Transfer", transferEventJSON)
+	if err != nil {
+		return fmt.Errorf("failed to set event: %v", err)
+	}
+
+	return nil
+}
