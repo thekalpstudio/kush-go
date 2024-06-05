b1546f09b524df39ace5796ab1517b8a477ea0a0
PrabalParihar <prabalparihar95@gmail.com>
2024-06-05 14:21:20 +0530

    feat: Add Initialize function to TokenERC20Contract
    Implement the Initialize function in the TokenERC20Contract to set the token name, symbol, and decimals. This function is only accessible to authorized clients and checks if the contract options are already set. It updates the world state with the provided values.

--- Contracts/token/ERC20.go
+++ Contracts/token/ERC20.go
@@ -1,8 +1,10 @@
 package token
 
 import (
-    "encoding/json"
-    "fmt"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"strconv"
     "github.com/p2eengineering/kalp-sdk-public/kalpsdk"
 )
 
@@ -16,4 +18,46 @@ const (
 
 type TokenERC20Contract struct {
 	kalpsdk.Contract
+}
+
+
+type event struct {
+	From  string `json:"from"`
+	To    string `json:"to"`
+	Value int    `json:"value"`
+}
+
+func (c *TokenERC20Contract) Initialize(ctx kalpsdk.TransactionContextInterface, name, symbol string, decimals int) (bool, error) {
+	clientMSPID, err := ctx.GetClientIdentity().GetMSPID()
+	if err != nil {
+		return false, fmt.Errorf("failed to get MSPID: %v", err)
+	}
+	if clientMSPID != "mailabs" {
+		return false, fmt.Errorf("client is not authorized to initialize contract")
+	}
+
+	bytes, err := ctx.GetState(nameKey)
+	if err != nil {
+		return false, fmt.Errorf("failed to get Name: %v", err)
+	}
+	if bytes != nil {
+		return false, fmt.Errorf("contract options are already set, client is not authorized to change them")
+	}
+
+	err = ctx.PutStateWithoutKYC(nameKey, []byte(name))
+	if err != nil {
+		return false, fmt.Errorf("failed to set token name: %v", err)
+	}
+
+	err = ctx.PutStateWithoutKYC(symbolKey, []byte(symbol))
+	if err != nil {
+		return false, fmt.Errorf("failed to set symbol: %v", err)
+	}
+
+	err = ctx.PutStateWithoutKYC(decimalsKey, []byte(strconv.Itoa(decimals)))
+	if err != nil {
+		return false, fmt.Errorf("failed to set decimals: %v", err)
+	}
+
+	return true, nil
 }
