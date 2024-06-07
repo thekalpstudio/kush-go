c8185678fc645bc498a30c00c6bea0f025c2d0db
PrabalParihar <prabalparihar95@gmail.com>
2024-06-07 14:40:06 +0530

    feat: Add event structs for ERC-1155 token transfers and approvals

--- Contracts/token/ERC1155.go
+++ Contracts/token/ERC1155.go
@@ -25,3 +25,37 @@ const symbolKey = "symbol"
 type SmartContract struct {
 	kalpsdk.Contract
 }
+
+// TransferSingle MUST emit when a single token is transferred, including zero
+// value transfers as well as minting or burning.
+type TransferSingle struct {
+	Operator string `json:"operator"`
+	From     string `json:"from"`
+	To       string `json:"to"`
+	ID       uint64 `json:"id"`
+	Value    uint64 `json:"value"`
+}
+
+// TransferBatch MUST emit when tokens are transferred, including zero value
+// transfers as well as minting or burning.
+type TransferBatch struct {
+	Operator string   `json:"operator"`
+	From     string   `json:"from"`
+	To       string   `json:"to"`
+	IDs      []uint64 `json:"ids"`
+	Values   []uint64 `json:"values"`
+}
+
+// ApprovalForAll MUST emit when approval for a second party/operator address
+// to manage all tokens for an owner address is enabled or disabled
+type ApprovalForAll struct {
+	Owner    string `json:"owner"`
+	Operator string `json:"operator"`
+	Approved bool   `json:"approved"`
+}
+
+// URI MUST emit when the URI is updated for a token ID.
+type URI struct {
+	Value string `json:"value"`
+	ID    uint64 `json:"id"`
+}
